306f420e7a3e2618303bbdf84366eda30a4b7a67
momongya <mo5e2ka8@gmail.com>
2022-12-27 15:16:58 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+
+require 'net/http'
+require 'uri'
+
+get '/' do
+    base_url = 'https://lit-wikipedia-api.herokuapp.com/?keyword='
+    if params[:keyword]
+        keyword = URI.encode_www_form_component(params[:keyword])
+        url = URI.parse(base_url + keyword)
+        @result = Net::HTTP.get(url).force_encoding("utf-8")
+    end
+    erb :index
+end
