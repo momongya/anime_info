7148700e04931d1685317d601db76f3180eb1bf8
momongya <mo5e2ka8@gmail.com>
2022-12-28 17:52:46 +0000

    表示検索できるようになった

--- app.rb
+++ app.rb
@@ -1,16 +1,21 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
-
+require 'date'
 require 'net/http'
 require 'uri'
 
 get '/' do
-    base_url = 'https://lit-wikipedia-api.herokuapp.com/?keyword='
-    if params[:keyword]
-        keyword = URI.encode_www_form_component(params[:keyword])
-        url = URI.parse(base_url + keyword)
-        @result = Net::HTTP.get(url).force_encoding("utf-8")
+    base_url = 'http://api.moemoe.tokyo/anime/v1/master/' + Date.today.year.to_s + '/' + Date.today.month.div(3).to_s
+    puts base_url
+    url = URI.parse(base_url)
+    returned_json = Net::HTTP.get(url).force_encoding("utf-8")
+    hash_data = JSON.parse(returned_json)  # RubyのHashに変換している
+    @result = []
+    hash_data.each do |res|
+        if res["title"].include?(params[:keyword])
+            @result.push(res["title"])
+        end
     end
     erb :index
 end
